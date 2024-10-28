1416e1c1f782cf3b7025a2fc53ee620ad53bc8ce
Shekhar Sahu <93962676+ShekharSahu404@users.noreply.github.com>
2024-10-28 23:45:47 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,8 +16,8 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "https://trac-for-sports-oooxjpx1y-shekhar-sahus-projects.vercel.app",
-      //["https://trac-for-sports.vercel.app","https://trac-for-sports-server.vercel.app"],
+    origin: 
+      ["https://trac-for-sports-oooxjpx1y-shekhar-sahus-projects.vercel.app","https://trac-for-sports-server.vercel.app"],
    
     credentials: true,
   })
