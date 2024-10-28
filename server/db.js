ec935a2bb6d8da57130d96786e3588eeb5669be6
Shekhar Sahu <93962676+ShekharSahu404@users.noreply.github.com>
2024-10-28 22:18:11 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
-//require("dotenv").config();
+require("dotenv").config();
 
 // Replace this with your MONGOURI.
-const MONGOURI = "mongodb+srv://shekharsahu1020:TQqo5DqD271w4Wro@talk.ltnqddt.mongodb.net/?retryWrites=true&w=majority&appName=talk";
+const MONGOURI = process.env.MONGO_URL;
+  //"mongodb+srv://shekharsahu1020:TQqo5DqD271w4Wro@talk.ltnqddt.mongodb.net/?retryWrites=true&w=majority&appName=talk";
 
-  //process.env.MONGO_URL;
+ 
 
 const InitiateMongoServer = async () => {
   try {
